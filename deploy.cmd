0dd11abb3cf2949e4705d1f7f15a1fa10b45be31
Alayande Abdulwaheed <alayandeabiola2013@gmail.com>
2018-04-02 12:21:43 +0100

    change in deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -83,8 +83,8 @@ pushd "%DEPLOYMENT_TARGET%"
 
 
 :: 3. Install packages
-echo Pip install requirements.
-env\Scripts\python.exe -m pip install --upgrade -r requirements.txt
+echo Pip3 install requirements.
+env\Scripts\python.exe -m pip3 install --upgrade -r requirements.txt
 IF !ERRORLEVEL! NEQ 0 goto error
 
 popd
